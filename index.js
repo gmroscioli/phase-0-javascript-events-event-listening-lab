87196faf49c705b25c6afc85bdad49d179e4d02c
Gina <gmroscioli@gmail.com>
2023-04-23 09:33:32 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+     alert('Hee hee that tickles!');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
